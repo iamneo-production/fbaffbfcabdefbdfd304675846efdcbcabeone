8584956d947a9267764895dabd0fe7129b4f309c
Iamneo <techadmin@iamneo.ai>
2023-09-03 12:15:41 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -2,7 +2,27 @@ let playerText=document.getElementById('playerText')
 let resetBtn=document.getElementById('resetBtn')
 let boxes=Array.from(document.getElementById('box'))
 
-const O_
+const O_TEXT="O"
+const X_TEXT="X"
+
+let currentPlayer=X_TEXT
+let spaces=Array(9).fill(null)
+
+const startGame=()=>{
+    boxes.forEach(box=>box.addEventListener('click',boxClicked))
+}
+
+function boxClicked(e)
+{
+    const id=e.target.id
+    if(!spaces[id]){
+        spaces[id]=currentPlayer
+        e.target.innetText=currentPlayer
+
+        currentPlayer=currentPlayer==X_TEXT?O_TEXT:X_TEXT
+    }
+}
+
 const conditions = [
     [0, 1, 2],
     [3, 4, 5],
