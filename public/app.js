1fb4df1cbcb046f089c0d9e738a32a5c957ea7a2
Iamneo <techadmin@iamneo.ai>
2023-09-03 13:32:54 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -18,12 +18,12 @@ function boxClicked(e)
         spaces[id]=currentPlayer
         e.target.innerText=currentPlayer
 
-        if(PlayerHasWon()!==false){
-            if(currentPlayer==X_TEXT)
-                result.innerHTML='Player X won!🎉'
-            else
-                result.innerHTML='Player O won!🎉'
-            currentPlayer=''
+        if(PlayerHasWon()!==false)
+        {
+            if(currentPlayer==X_TEXT){
+                result.innerHTML='Player X won!🎉'}
+            else{
+                result.innerHTML='Player O won!🎉'}
         }
         
         currentPlayer=currentPlayer==X_TEXT?O_TEXT:X_TEXT
@@ -65,53 +65,3 @@ function reset(){
     result.innerHTML=''
 }
 startGame()
-
-
-// // Function to handle player moves
-// const ticTacToe = (element, index) => {
-//     // Your game logic here
-
-//     /*
-//     **Part 1: Winning Conditions (Add your code here)**
-
-//     1. Implement the logic to check for winning conditions using the 'conditions' array.
-//     2. Display a winning message in the 'result' element when a player wins.
-//     3. Disable all buttons after a win.
-//     */
-
-//     // Your code to update the game state and check for a win
-//     // ...
-
-//     // Your code to display the current player's turn
-//     // ...
-
-//     // Your code to handle button and cell interactions
-//     // ...
-// };
-
-//     /*
-//     **Part 2: Reset Function (Add your code here)**
-
-//     1. Implement a new function that resets the game to its initial state.
-//     2. Ensure the 'cells', 'btns', and 'currentPlayer' variables are reset.
-//     3. Update the 'result' element to indicate the current player's turn.
-//     4. Re-enable all buttons for a new game.
-//     */
-
-// // Function to reset the game
-// const resetGame = () => {
-//     // Your code to reset the game state
-//     // ...
-
-//     // Your code to update the 'result' element
-//     // ...
-
-//     // Your code to re-enable buttons
-//     // ...
-// };
-
-// btns.forEach((btn, i) => {
-//     btn.addEventListener('click', () => ticTacToe(btn, i));
-// });
-
-// document.querySelector('#reset').addEventListener('click', resetGame);
